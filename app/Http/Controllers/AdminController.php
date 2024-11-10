0362848ef16fa5dce59d62d9da2be9e40f537e15
Abdullah Onur Keklik <ao_keklik@hotmail.com>
2024-11-10 18:35:39 +0100

    feat: add displaying discounted (sale) and featured products on homepage

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -4,10 +4,68 @@ namespace App\Http\Controllers;
 
 use App\Models\Order;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
 
 class AdminController {
     public function index () {
         $orders = Order::where("user_id", Auth::user()->id)->orderBy("id", "DESC")->get()->take(5);
-        return view ("admin.index", compact('orders'));
+
+        $dashboardData = DB::select("select sum(total) TotalAmount, 
+        sum(if(status='ordered',total,0)) as TotalOrderedAmount,
+        sum(if(status='delivered',total,0)) as TotalDeliveredAmount,
+        sum(if(status='canceled',total,0)) as TotalCanceledAmount,
+        count(*) as Total,
+        sum(if(status='ordered',1,0)) as TotalOrdered,
+        sum(if(status='delivered',1,0)) as TotalDelivered,
+        sum(if(status='canceled',1,0)) as TotalCanceled
+        from orders");
+
+        $motnhlyData = DB::select("select 
+        M.id as MonthNo, 
+        M.name as MonthName,
+        ifnull(D.TotalAmount,0) as TotalAmount,
+        ifnull(D.TotalOrderedAmount,0) as TotalOrderedAmount,
+        ifnull(D.TotalDeliveredAmount,0) as TotalDeliveredAmount,
+        ifnull(D.TotalCanceledAmount,0) as TotalCanceledAmount
+        from month_names as M
+
+        left join (select 
+        date_format(created_at, '%b') MonthName,
+        sum(total) as TotalAmount,
+        sum(if(status='ordered',total,0)) as TotalOrderedAmount,
+        sum(if(status='delivered',total,0)) as TotalDeliveredAmount,
+        sum(if(status='canceled',total,0)) as TotalCanceledAmount,
+        month(created_at) as MonthNo
+        from orders
+        where year(created_at)=year(now()) 
+        group by year(created_at), month(created_at), date_format(created_at, '%b')
+        order by month(created_at)
+        
+        ) as D on D.MonthNo=M.id");
+
+        $amountM = implode(",", collect($motnhlyData)->pluck("TotalAmount")->toArray());
+        $orderAmountM = implode(",", collect($motnhlyData)->pluck("TotalOrderedAmount")->toArray());
+        $deliveredAmountM = implode(",", collect($motnhlyData)->pluck("TotalDeliveredAmount")->toArray());
+        $canceledAmountM = implode(",", collect($motnhlyData)->pluck("TotalCanceledAmount")->toArray());
+
+        $totalAmount = collect($motnhlyData)->sum("TotalAmount");
+        $totalOrderdAmount = collect($motnhlyData)->sum("TotalOrderedAmount");
+        $totalDeliveredAmount = collect($motnhlyData)->sum("TotalDeliveredAmount");
+        $totalCanceledAmount = collect($motnhlyData)->sum("TotalCanceledAmount");
+
+
+        return view ("admin.index", compact(
+            "orders", 
+            "dashboardData", 
+            "motnhlyData",
+            "amountM",
+            "orderAmountM",
+            "deliveredAmountM",
+            "canceledAmountM",
+            "totalAmount",
+            "totalOrderdAmount",
+            "totalDeliveredAmount",
+            "totalCanceledAmount"
+        ));
     }
 }
--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Category;
+use App\Models\Product;
 use App\Models\Slide;
 use Illuminate\Http\Request;
 
@@ -10,6 +12,13 @@ class HomeController extends Controller
     public function index()
     {
         $slides = Slide::where("status", 1)->orderBy("id", "DESC")->get()->take(3);
-        return view('index', compact("slides"));
+        $categories = Category::orderBy("name", "ASC")->get();
+        $products = Product::whereNotNull("sale_price")
+            ->where("sale_price", "<>", "0")
+            ->inRandomOrder()
+            ->get()->take(8);
+        $fProducts = Product::where("featured", 1)->orderBy("created_at", "DESC")->get()->take(8);
+
+        return view('index', compact("slides", "categories", "products", "fProducts"));
     }
 }
